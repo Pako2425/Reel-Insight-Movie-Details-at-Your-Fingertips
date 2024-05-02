8df9383e7bd6b52e910dc448b9a738526b045cb0
Pako2425 <patryk.kocinski340@gmail.com>
2024-05-03 00:31:32 +0200

    Results page - TODO: add clicking on poster and then moving to Movie Details Page.

--- App.tsx
+++ App.tsx
@@ -5,6 +5,48 @@ import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const movies = [];
+const moviesTest = [{
+                        "title": "The Avengers",
+                        "year": "2012",
+                        "id": "tt0848228",
+                        "type": "movie",
+                        "poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+                    },
+                    {
+                        "title": "Avengers: Endgame",
+                        "year": "2019",
+                        "id": "tt4154796",
+                        "type": "movie",
+                        "poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
+                    },
+                    {
+                        "title": "Avengers: Infinity War",
+                        "year": "2018",
+                        "id": "tt4154756",
+                        "type": "movie",
+                        "poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+                    },
+                    {
+                        "title": "Avengers: Age of Ultron",
+                        "year": "2015",
+                        "id": "tt2395427",
+                        "type": "movie",
+                        "poster": "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg"
+                    },
+                    {
+                        "title": "The Avengers",
+                        "year": "1998",
+                        "id": "tt0118661",
+                        "type": "movie",
+                        "poster": "https://m.media-amazon.com/images/M/MV5BZTQ4NmIzMTktOTdjOC00NzE4LWIzNTgtODkwNzM5ZjUzZDkxXkEyXkFqcGdeQXVyMTUzMDUzNTI3._V1_SX300.jpg"
+                    },
+                    {
+                        "title": "The Avengers: Earth's Mightiest Heroes",
+                        "year": "2010–2012",
+                        "id": "tt1626038",
+                        "type": "series",
+                        "poster": "https://m.media-amazon.com/images/M/MV5BYzA4ZjVhYzctZmI0NC00ZmIxLWFmYTgtOGIxMDYxODhmMGQ2XkEyXkFqcGdeQXVyNjExODE1MDc@._V1_SX300.jpg"
+                    }];
 
 function MainPage({navigation}) {
     const [searchBarText, setSearchBarText] = useState("");
@@ -54,10 +96,10 @@ function ResultsPage(navigation) {
 
     const renderItem = ({item}) => {
         return(
-            <View>
-                <Image source={{uri: item.poster}} style={styles.poster} resizeMode='contain'/>
-                <Text style={styles.text}>{item.title}</Text>
-                <Text style={styles.text}>{item.year}</Text>
+            <View style={styles.poster}>
+                <Image source={{uri: item.poster}} style={styles.posterImage} resizeMode='contain'/>
+                <Text style={styles.posterText}>{item.title}</Text>
+                <Text style={styles.posterText}>{item.year}</Text>
             </View>
         );
     }
@@ -65,6 +107,7 @@ function ResultsPage(navigation) {
     return(
         <View style={styles.container}>
             <FlatList
+
                 data={movies}
                 renderItem={renderItem}
                 keyExtractor={item => item.id}
@@ -82,6 +125,7 @@ function App(): React.JSX.Element {
             <Stack.Navigator>
                 <Stack.Screen name="MainPage" component={MainPage}/>
                 <Stack.Screen name="ResultsPage" component={ResultsPage}/>
+
             </Stack.Navigator>
         </NavigationContainer>
     );
@@ -126,15 +170,23 @@ const styles = StyleSheet.create({
 
     poster: {
         width: 150,
-        height: 300,
-        marginRight: 13
+        height: 'auto',
+        marginRight: 14,
+        marginLeft: 14,
+        marginBottom: 10,
+        justifyContent: 'center',
     },
 
-    postersRow: {
-        flexDirection: 'row',
-        marginTop: 10,
-        marginBottom: 0
+    posterImage: {
+        width: 150,
+        height: 250
+    },
+
+    posterText: {
+        fontSize: 16,
+        color: 'white'
     }
+
 });
 
 export default App;
