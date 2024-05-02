7de5051823398c5c51f64ebef0f9dfc07681195e
Pako2425 <patryk.kocinski340@gmail.com>
2024-05-02 11:26:21 +0200

    Asking, receiving and rendering results working v1.0.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
 import React, {useState} from 'react';
 
-import {View, Text, StyleSheet, TextInput, TouchableHighlight} from 'react-native'
-import MainPage from './MainPage.jsx'
+import {View, Text, StyleSheet, TextInput, TouchableHighlight, Button, Image, ScrollView, FlatList} from 'react-native'
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function App(): React.JSX.Element {
+const movies = [];
 
+function MainPage({navigation}) {
     const [searchBarText, setSearchBarText] = useState("");
-    const [apiRequest, setApiRequest] = useState("");
-    const [film, setFilm] = useState("");
 
     async function getData(movieTitle) {
-        let apiRequest = 'http://www.omdbapi.com/?apikey=c9bb574f&s=' + movieTitle;
-        try{
+        let apiRequest = 'http://www.omdbapi.com/?apikey=c9bb574f&s=' + movieTitle.toLowerCase();
+        try {
             let response = await fetch(apiRequest);
-            let data = await response.json();
-            await setFilm(data["Search"]["1"]["Year"]);
-            await alert(film);
+            data = await response.json();
+            data.Search.forEach(movie => movies.push(
+                {
+                    id: movie["imdbID"],
+                    title: movie["Title"],
+                    year: movie["Year"],
+                    poster: movie["Poster"]
+                }
+            ));
+            navigation.navigate("ResultsPage");
         }
         catch(error) {
             alert(`Can't find your movie: \"${searchBarText}\"
@@ -40,6 +47,43 @@ function App(): React.JSX.Element {
                 <Text style={{color: 'white', fontSize: 15}}>Search</Text>
             </TouchableHighlight>
         </View>
+    );
+}
+
+function ResultsPage(navigation) {
+
+    const renderItem = ({item}) => {
+        return(
+            <View>
+                <Image source={{uri: item.poster}} style={styles.poster} resizeMode='contain'/>
+                <Text style={styles.text}>{item.title}</Text>
+                <Text style={styles.text}>{item.year}</Text>
+            </View>
+        );
+    }
+
+    return(
+        <View style={styles.container}>
+            <FlatList
+                data={movies}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                numColumns={2}
+            />
+        </View>
+    );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App(): React.JSX.Element {
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="MainPage" component={MainPage}/>
+                <Stack.Screen name="ResultsPage" component={ResultsPage}/>
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
@@ -78,6 +122,18 @@ const styles = StyleSheet.create({
         margin: 14,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+
+    poster: {
+        width: 150,
+        height: 300,
+        marginRight: 13
+    },
+
+    postersRow: {
+        flexDirection: 'row',
+        marginTop: 10,
+        marginBottom: 0
     }
 });
 
